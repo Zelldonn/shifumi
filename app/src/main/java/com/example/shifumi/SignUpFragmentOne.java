666bf869d2868119b350424a56868917159f2008
pierre <stringaripierre@hotmail.fr>
2021-10-21 18:21:40 +0200

    esiee work

---
+++ app/src/main/java/com/example/shifumi/SignUpFragmentOne.java
@@ -0,0 +1,21 @@
+package com.example.shifumi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SignUpFragmentOne extends Fragment {
+    View v;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_sign_up_one, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/shifumi/SignUpFragmentTwo.java
@@ -0,0 +1,22 @@
+package com.example.shifumi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SignUpFragmentTwo extends Fragment {
+
+    View v;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_sign_up_two, container, false);
+        return v;
+    }
+}
